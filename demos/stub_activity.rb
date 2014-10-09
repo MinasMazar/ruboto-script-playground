071c145022916262cdc2871e4c3673ee9d201d4f
MinasMazar <minasmazar@gmail.com>
2014-10-09 12:28:44 +0200

    Promote previous stub to LoveRuby Script and start new stub activity

--- demos/stub_activity.rb
+++ demos/stub_activity.rb
@@ -1,6 +1,12 @@
 require 'ruboto/widget'
 require 'ruboto/util/toast'
 
+java_import 'android.content.res.AssetManager'
+java_import 'android.text.InputType'
+java_import 'java.io.BufferedReader'
+java_import 'java.io.InputStreamReader'
+java_import 'java.io.FileNotFoundException'
+
 ruboto_import_widgets :Button, :LinearLayout, :TextView
 
 start_ruboto_activity do
@@ -8,9 +14,25 @@ start_ruboto_activity do
     super
     set_title 'Stub Ruboto Activity'
     self.content_view = linear_layout :orientation => :vertical do
-      text_view :text => "I Love Ruby <3", :layout => {:width => :match_parent}, :gravity => :center, :text_size => 48.0
+      text_view :text => '', :layout => {:width => :match_parent}, :gravity => :center
+      @word = edit_text :text => '', :hint => "Word (ex. COL??E?TA)"
+      button :text => 'Cruciforce!', :on_click_listener => proc { cruciforce }
     end
   end
 
   ## Other unique activity code
+  
+  def cruciforce
+    @word.text
+    begin
+      br = BufferedReader.new (InputStreamReader.new (getAssets.open("demo-scripts/dict/#{@lang}_#{wc}.dict")))
+    rescue FileNotFoundException
+      @result_pane.notify "Dictionary not found for #{wc} words lenght"
+    end
+    puts br.inspect
+    return
+    while (l = br.readLine.chomp)
+      
+    end
+  end
 end
