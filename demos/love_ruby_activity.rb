ad0668aaa370164f33b01ad16a6738e2483c1f8a
MinasMazar <minasmazar@gmail.com>
2015-02-25 17:27:29 +0100

    Add sound support to I<3Ruby

--- demos/love_ruby_activity.rb
+++ demos/love_ruby_activity.rb
@@ -4,12 +4,27 @@ require 'ruboto/util/toast'
 ruboto_import_widgets :Button, :LinearLayout, :TextView
 
 start_ruboto_activity do
+
   def on_create(bundle)
     super
     set_title 'Stub Ruboto Activity'
     self.content_view = linear_layout :orientation => :vertical do
       text_view :text => "I Love Ruby <3", :layout => {:width => :match_parent}, :gravity => :center, :text_size => 48.0
     end
+    @file = './hangout_dingtone.m4a'
+    play_file @file
+  end
+
+  def play_file(file)
+    request_callback CB_PAUSE
+    file = File.new(file)
+    uri = Uri.from_file(file)
+    @player = MediaPlayer.create(self, uri)
+    @player.start
+  end
+
+  def on_pause
+    @player.stop
   end
 
   ## Other unique activity code
